07505eaa0b3370c77c47672d7afb56aebd2327e8
matheus miranda <matheus-m-ml@hotmal.com>
2015-09-17 02:37:29 -0300

    Creating Superior_Public_Agency model

---
+++ Aonde/app/models/superior_public_agency.rb
@@ -0,0 +1,2 @@
+class SuperiorPublicAgency < ActiveRecord::Base
+end
---
+++ Aonde/test/models/superior_public_agency_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SuperiorPublicAgencyTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
