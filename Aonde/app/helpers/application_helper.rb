422325236379fe0f3d8becbe4f990d3a032f88cb
matheus miranda <matheus-m-ml@hotmal.com>
2015-09-17 12:41:43 -0300

    Modify some files

--- Aonde/app/helpers/application_helper.rb
+++ Aonde/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(title = "")
+		title_default = "AondE"
+		if not title.empty?
+			return title_default + " - " + title
+		else
+			return title_default
+		end
+	end
 end
