295029b757ce4164bbdf28cb8cfba23bc7ffcabb
matheus miranda <matheus-m-ml@hotmal.com>
2015-09-17 02:52:19 -0300

    Adding attributes in Public_Agency table

--- Aonde/db/migrate/20150917054547_create_public_agencies.rb
+++ Aonde/db/migrate/20150917054547_create_public_agencies.rb
@@ -1,9 +1,13 @@
 class CreatePublicAgencies < ActiveRecord::Migration
   def change
     create_table :public_agencies do |t|
-      t.references :superior_public_agency, index: true, foreign_key: true
 
-      t.timestamps null: false
+    t.references :superior_public_agency, index: true, foreign_key: true
+    t.string name:
+    t.integer views_amount:
+    
+    t.timestamps null: false
+
     end
   end
 end
