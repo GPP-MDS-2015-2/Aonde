e4a74719c609cd641625e3f4df8d89f6d2bb7bc3
Marcelo Ferreira <marcelohpf@gmail.com>
2015-09-17 15:00:37 -0300

    Modify the layout
    Modify the layout of the all pages

--- Aonde/config/routes.rb
+++ Aonde/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-
-  root 'public_agency#index'
+  get "welcome/index" => "welcome#index"
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- Aonde/db/migrate/20150917054314_create_public_agencies.rb
+++
@@ -1,8 +0,0 @@
-class CreatePublicAgencies < ActiveRecord::Migration
-  def change
-    create_table :public_agencies do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
