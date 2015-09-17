8eee4c1636fc4aca8e51d639e75db29553e8c364
matheus miranda <matheus-m-ml@hotmal.com>
2015-09-17 02:41:10 -0300

    Adding attribute to Superior_Public_Agency

--- Aonde/db/migrate/20150917053658_create_superior_public_agencies.rb
+++ Aonde/db/migrate/20150917053658_create_superior_public_agencies.rb
@@ -2,7 +2,9 @@ class CreateSuperiorPublicAgencies < ActiveRecord::Migration
   def change
     create_table :superior_public_agencies do |t|
 
-      t.timestamps null: false
+    t.string name:
+
+    t.timestamps null: false
     end
   end
 end
