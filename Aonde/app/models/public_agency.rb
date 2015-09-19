46c4fc5945fa4beda5740f8fbe3c3719c10a8a09
ArthurTemporim <arthurrl@gmai.com>
2015-09-19 16:18:31 -0300

    Correct made on test classes. Include validation on PublicAgency

--- Aonde/app/models/public_agency.rb
+++ Aonde/app/models/public_agency.rb
@@ -1,4 +1,12 @@
-class PublicAgency < ActiveRecord::Base
+﻿class PublicAgency < ActiveRecord::Base
   belongs_to :superior_public_agency
   has_many :program
+
+#including validation.
+  validates :name, presence: true
+  validates :views_amount, length: {minimum: 0}
+ 
+
+PublicAgency.create(name: "Ministerio da Fazenda", views_amount: 10).valid? # => true
+PublicAgency.create(name: nil, views_amount: 0).valid? # => false
 end
--- Aonde/test/models/public_agency_test.rb
+++ Aonde/test/models/public_agency_test.rb
@@ -5,12 +5,12 @@ require 'test_helper'
 class PublicAgencyTest < ActiveSupport::TestCase
 
   def test_public_agency
-  	public_agency = Public_agency.new(:name => public_agencies(:ministerio_da_fazenda).name, :views_amount => public_agencies(:ministerio_da_fazenda).views_amount)
+  	public_agency = PublicAgency.new(:name => public_agencies(:three).name, :views_amount => public_agencies(:three).views_amount)
 
   	#HAVE A MISTAKE ONLY IN THE NEXT LINE.
   	msg = "public_agency wasn't saved. " + "erros: ${public_agency.errors.inspect}"# assert public_agency.save, msg
 
-  	public_agency_ministerio_da_fazenda_copy = Public_agency.find(public_agency.id)
+  	public_agency_ministerio_da_fazenda_copy = PublicAgency.find(2)
   	assert_equal public_agency.name, public_agency_ministerio_da_fazenda_copy.name
   end
   # test "the truth" do
