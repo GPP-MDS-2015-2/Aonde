d55e6ac95b8fd65d6bb000a6605894116080955a
ArthurTemporim <arthurrl@gmai.com>
2015-09-19 16:55:49 -0300

    Made a usefull test on test folder

--- Aonde/test/models/public_agency_test.rb
+++ Aonde/test/models/public_agency_test.rb
@@ -4,16 +4,11 @@ require 'test_helper'
 
 class PublicAgencyTest < ActiveSupport::TestCase
 
-  def test_public_agency
+  def test_public_agency_save
   	public_agency = PublicAgency.new(:name => public_agencies(:three).name, :views_amount => public_agencies(:three).views_amount)
 
-  	#HAVE A MISTAKE ONLY IN THE NEXT LINE.
-  	msg = "public_agency wasn't saved. " + "erros: ${public_agency.errors.inspect}"# assert public_agency.save, msg
-
-  	public_agency_ministerio_da_fazenda_copy = PublicAgency.find(2)
-  	assert_equal public_agency.name, public_agency_ministerio_da_fazenda_copy.name
+    #assert_not public_agency.valid?
+    assert public_agency.valid?
+    assert public_agency.save
   end
-  # test "the truth" do
-  #   assert true
-  # end
 end
