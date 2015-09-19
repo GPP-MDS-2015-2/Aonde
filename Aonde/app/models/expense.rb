fc18cb16d70c3412f0834a8728bdcf7192a981c0
Artur <arturbersan@gmail.com>
2015-09-19 16:01:00 -0300

    Create validates for Expense model

--- Aonde/app/models/expense.rb
+++ Aonde/app/models/expense.rb
@@ -1,3 +1,6 @@
 class Expense < ActiveRecord::Base
-  belongs_to :program
+  belongs_to :program 
+  validates :document_number, uniqueness: true,on: :create,presence: true
+  validates :payment_date, presence: true,uniqueness: true,on: :create
+
 end
