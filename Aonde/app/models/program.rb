b6b3157a68abcba6231fcd79c1bf8876ed6f49bb
ArthurTemporim <arthurrl@gmai.com>
2015-09-19 16:31:30 -0300

    Validation made on program.

--- Aonde/app/models/program.rb
+++ Aonde/app/models/program.rb
@@ -1,4 +1,6 @@
 class Program < ActiveRecord::Base
   belongs_to :public_agency
   has_many :expense
+
+  validates :name, presence: true
 end
--- Aonde/app/models/public_agency.rb
+++ Aonde/app/models/public_agency.rb
@@ -5,8 +5,4 @@
 #including validation.
   validates :name, presence: true
   validates :views_amount, length: {minimum: 0}
- 
-
-PublicAgency.create(name: "Ministerio da Fazenda", views_amount: 10).valid? # => true
-PublicAgency.create(name: nil, views_amount: 0).valid? # => false
 end
