10dc4b0ca3a42af9859c36dc6b6082290815951d
matheus miranda <matheus-m-ml@hotmal.com>
2015-09-19 19:46:31 -0300

    Add method to generate totalExpense

--- Aonde/app/helpers/public_agency_helper.rb
+++ Aonde/app/helpers/public_agency_helper.rb
@@ -1,2 +1,14 @@
 module PublicAgencyHelper
-end
+	def expenses_public_agency(id_pub_agency)
+	  	total_expense = 0
+	  	program = Program.where(public_agency_id: id_pub_agency)
+	  	program.each do |prog|
+	  		expenses = Expense.where(program_id: prog.id)
+	  		expenses.each do |exp|
+	  			total_expense += exp.value
+	  		end	
+	  	end
+	  	return total_expense
+	end
+end	
+
--- Aonde/app/models/expense.rb
+++ Aonde/app/models/expense.rb
@@ -2,5 +2,4 @@ class Expense < ActiveRecord::Base
   belongs_to :program 
   validates :document_number, uniqueness: true,on: :create,presence: true
   validates :payment_date, presence: true,uniqueness: true,on: :create
-
 end
