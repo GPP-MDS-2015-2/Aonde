52d536dbcf34d4856e3209b5fd3fcab632aa4c9b
Artur Bersan de Faria <artur_bersan@hotmail.com>
2015-09-23 13:58:52 -0300

    Create method get_list_expense_month

--- Aonde/app/controllers/public_agency_controller.rb
+++ Aonde/app/controllers/public_agency_controller.rb
@@ -16,7 +16,23 @@ class PublicAgencyController < ApplicationController
 
 		@public_agency = PublicAgency.find(params[:id])
 		@superior_public_agency = SuperiorPublicAgency.find(@public_agency.superior_public_agency_id)
+		@list_expense_month = get_list_expense_month(params[:id])
 
+	end
+
+	def get_list_expense_month(id_public_agency)
+	  	total_expense_per_date = [["ano","gasto"]]
+	  	programs = Program.where(public_agency_id: id_public_agency)
+	  	programs.each do |prog|
+	  		expenses = Expense.where(program_id: prog.id)
+	  		expenses.each do |exp|
+	  			month = Date::MONTHNAMES[exp.payment_date.month]
+	  			year = exp.payment_date.year
+	  			expense_info = ["#{month}/#{year}",exp.value] 
+	  			total_expense_per_date << expense_info
+	  		end	
+	  	end
+	  	return total_expense_per_date
 	end
 
 end
