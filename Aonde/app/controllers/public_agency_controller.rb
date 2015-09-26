1ea734f889fc02e180d755f38804befdcc3d6670
Artur Bersan de Faria <artur_bersan@hotmail.com>
2015-09-25 21:23:05 -0300

    Fix order of graph dates

--- Aonde/app/controllers/public_agency_controller.rb
+++ Aonde/app/controllers/public_agency_controller.rb
@@ -8,7 +8,7 @@ class PublicAgencyController < ApplicationController
 	def show
 		@public_agency = PublicAgency.find(params[:id])
 		@superior_public_agency = SuperiorPublicAgency.find(@public_agency.superior_public_agency_id)
-		@list_expense_month = @public_agency.get_list_expense_month
+		@list_expense_month = get_list_expense_month(params[:id])
 		@list_expense_month.unshift(["Data","gasto"])
 		increment_views_amount
 	end
@@ -28,9 +28,27 @@ class PublicAgencyController < ApplicationController
 	  	return total_expense_per_date_by_period.to_a	  	
 	end
 	
+
 	def increment_views_amount	
 		views_amount = @public_agency.views_amount
 		views_amount += 1
 		@public_agency.update(views_amount: views_amount)		
 	end
+
+
+	def get_list_expense_month(id_public_agency)
+    total_expense_per_date = {}
+    #Takes all programs and return a list
+      expenses = Expense.where(program_id: Program.where(public_agency_id: id_public_agency).ids)
+      expenses.each do |exp|
+        date = l(Date.new(exp.payment_date.year,exp.payment_date.month,1))
+        if total_expense_per_date [date] == nil
+          total_expense_per_date [date] = 0
+        end
+        total_expense_per_date [date] += exp.value
+      end 
+    return total_expense_per_date.sort_by { |date, expenses| Date.parse(date) }.to_a
+  end
+
+
 end
--- Aonde/app/models/public_agency.rb
+++ Aonde/app/models/public_agency.rb
@@ -6,20 +6,4 @@
   validates :name, presence: true
   validates :views_amount, numericality: {greater_than: 0}
 
-  def get_list_expense_month
-      total_expense_per_date = {}
-      programs = Program.where(public_agency_id: self.id)
-      programs.each do |prog|
-        expenses = Expense.where(program_id: prog.id)
-        expenses.each do |exp|
-          date = Date.new(exp.payment_date.year,exp.payment_date.month,1)
-          if total_expense_per_date [date] == nil
-            total_expense_per_date [date] = 0
-          end
-          total_expense_per_date [date] += exp.value
-        end 
-      end
-      return total_expense_per_date.sort_by { |date, expenses| date }.to_a
-  end
-
 end
