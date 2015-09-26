89173956cbf22e37b81a0d13a110652535ebb4ff
Marcelo Ferreira <marcelohpf@gmail.com>
2015-09-25 21:53:55 -0300

    Add the method to filter the expenses

--- Aonde/app/controllers/public_agency_controller.rb
+++ Aonde/app/controllers/public_agency_controller.rb
@@ -8,26 +8,12 @@ class PublicAgencyController < ApplicationController
 	def show
 		@public_agency = PublicAgency.find(params[:id])
 		@superior_public_agency = SuperiorPublicAgency.find(@public_agency.superior_public_agency_id)
-		@list_expense_month = get_list_expense_month(params[:id])
+		@list_expense_month = get_list_expense_month(params[:id]).to_a
 		@list_expense_month.unshift(["Data","gasto"])
 		increment_views_amount
 	end
 
 	#Calculate by month/year the total of expense
-	
-  	def get_list_expenses_by_period(id_public_agency,month_init,year_init,month_final,year_month)
-	
-		total_expense_per_date = list_expense_month(id_public_agency)
-		total_expense_per_date.each do |date,valor|
-			if date.year.to_i >= year_init && date.year.to_i <= year_final
-		  		if date.month.to_i >= month_init && date.month.to_i <= month_final
-		  			total_expense_per_date [date] += exp.value
-		  		end
-		  	end
-		end
-	  	return total_expense_per_date_by_period.to_a	  	
-	end
-	
 
 	def increment_views_amount	
 		views_amount = @public_agency.views_amount
@@ -50,5 +36,50 @@ class PublicAgencyController < ApplicationController
     return total_expense_per_date.sort_by { |date, expenses| Date.parse(date) }.to_a
   end
 
+def filter_chart
+  		#find the same thing then the show
+  		@public_agency = PublicAgency.find(params[:id])
+  		@superior_public_agency = SuperiorPublicAgency.find(@public_agency.superior_public_agency_id)
+  		#create the new list with filters apllied
+  		@list_expense_month = get_list_expenses_by_period(@public_agency.id,params[:from_months],params[:from_year],params[:ends_in_the_months],params[:ends_in_the_year])
+  		#insert the head in the list
+  		@list_expense_month.unshift(["Data","Gasto"])
+		render 'show'
+  	end
 
+  	def get_list_expenses_by_period(id_public_agency,month_init,year_init,month_final,year_final)
+  		#create a hash to convert a name of month to int
+		conversion_data = {}
+		conversion_data['Janeiro'] = 1
+		conversion_data['Fevereiro'] = 2
+		conversion_data['Março'] = 3
+		conversion_data['Abril'] = 4
+		conversion_data['Maio'] = 5
+		conversion_data['Junho'] =6
+		conversion_data['Julho'] = 7
+		conversion_data['Agosto'] = 8
+		conversion_data['Setembro'] = 9
+		conversion_data['Outubro'] = 10
+		conversion_data['Novembro'] = 11
+		conversion_data['Dezembro'] = 12
+		
+		
+		#create a new hash to put the values
+		new_total_expense_per_date = {}
+		#take all of expenses
+		expenses = Expense.where(program_id: Program.where(public_agency_id: id_public_agency).ids)
+      	expenses.each do |exp|
+			date = exp.payment_date
+			#see if the date are in the hash and add in the new	
+			#verifyy if the date are in the interval
+			if date.year.to_i >= year_init.to_i && date.year.to_i <= year_final.to_i
+		  		if date.month.to_i >= conversion_data[month_init] && date.month.to_i <= conversion_data[month_final]
+		  			#create the date in the new hash, but the key is a string
+	        		new_total_expense_per_date [date.to_s] = exp.value
+		  		end
+		  	end
+		end
+	  	#return the hash with expenses like a array
+	  	return new_total_expense_per_date.to_a
+	end
 end
