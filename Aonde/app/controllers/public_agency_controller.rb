9211cc376d345117dc976186df45cbda4b71d95a
arthurTemporim <arthurrt@gmail.com>
2015-09-29 00:52:29 -0300

    Add method month_to_int, remove parts thats not necessary anymore

--- Aonde/app/controllers/public_agency_controller.rb
+++ Aonde/app/controllers/public_agency_controller.rb
@@ -49,8 +49,9 @@ def filter_chart
 		render 'show'
   	end
 
-  	def get_list_expenses_by_period(id_public_agency,month_init,year_init,month_final,year_final)
-  		#create a hash to convert a name of month to int
+	#create a hash to convert a name of month to int
+  	def month_to_int(month)
+	
 		conversion_data = {}
 		conversion_data['Janeiro'] = 1
 		conversion_data['Fevereiro'] = 2
@@ -64,19 +65,25 @@ def filter_chart
 		conversion_data['Outubro'] = 10
 		conversion_data['Novembro'] = 11
 		conversion_data['Dezembro'] = 12
+
+		return conversion_data[month]
+	end
+
+  	def get_list_expenses_by_period(id_public_agency,first_month,first_year,last_month,last_year)
+
 		@total_expense = 0
 
 		#create a new hash to put the values
 		new_total_expense_per_date = {}
 		#take all of expenses
 		expenses = Expense.where(program_id: Program.where(public_agency_id: id_public_agency).ids)
-      	if(valide_date(year_init,year_final,month_init,month_final))
+      	if(valide_date(first_year,last_year,first_month,last_month))
 	      	expenses.each do |exp|
 				date = exp.payment_date
 				#see if the date are in the hash and add in the new	
 				#verifyy if the date are in the interval
-				if date.year.to_i >= year_init.to_i && date.year.to_i <= year_final.to_i
-			  		if date.month.to_i >= conversion_data[month_init] && date.month.to_i <= conversion_data[month_final]
+				if date.year.to_i >= first_year.to_i && date.year.to_i <= last_year.to_i
+			  		if date.month.to_i >= month_to_int(first_month) && date.month.to_i <= month_to_int(last_month)
 		        		date = l(Date.new(exp.payment_date.year,exp.payment_date.month,1))
 		        		if(new_total_expense_per_date[date] == nil)
 		        			new_total_expense_per_date [date] = 0
@@ -99,27 +106,17 @@ def filter_chart
 	  	#return the hash with expenses like a array
 	  	return new_total_expense_per_date.sort_by { |date, expenses| Date.parse(date) }.to_a
 	end
-	def valide_date(ano_inicio,ano_fim,mes_inicio,mes_fim)
-		conversion_data = {}
-		conversion_data['Janeiro'] = 1
-		conversion_data['Fevereiro'] = 2
-		conversion_data['Março'] = 3
-		conversion_data['Abril'] = 4
-		conversion_data['Maio'] = 5
-		conversion_data['Junho'] =6
-		conversion_data['Julho'] = 7
-		conversion_data['Agosto'] = 8
-		conversion_data['Setembro'] = 9
-		conversion_data['Outubro'] = 10
-		conversion_data['Novembro'] = 11
-		conversion_data['Dezembro'] = 12
-		if(ano_inicio == ano_fim)
-			if(conversion_data[mes_inicio] > conversion_data[mes_fim])
+	def valide_date(first_year,last_year,first_month,last_month)
+
+		if(first_year == last_year)
+			if(month_to_int(first_month) > month_to_int(last_month))
 				return false
 			end
-		elsif (ano_inicio > ano_fim)
+		elsif (first_year > last_year)
 			return false
 		end
 		return true
+
 	end
+	
 end
