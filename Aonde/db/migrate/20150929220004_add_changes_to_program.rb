0e20627fbb4fdd92a4e559d9e27ee073159fd116
matheus miranda <matheus-m-ml@hotmal.com>
2015-09-30 19:48:35 -0300

    Fix slight changes

--- Aonde/db/migrate/20150929220004_add_changes_to_program.rb
+++ Aonde/db/migrate/20150929220004_add_changes_to_program.rb
@@ -4,12 +4,9 @@ class AddChangesToProgram < ActiveRecord::Migration
     
       remove_foreign_key :programs, :public_agencies
       remove_index :programs, :public_agency_id
-#Mistake made on remove id column of public_agency
-#      t.remove :public_agency_id
+      t.remove :public_agency_id
       t.string :description
 
-      
-      
     end
   end
 end
--- Aonde/db/schema.rb
+++ Aonde/db/schema.rb
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 20150930002126) do
   create_table "programs", force: :cascade do |t|
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
-    t.integer  "public_agency_id", limit: 4
     t.string   "name",             limit: 255
     t.string   "description",      limit: 255
   end
@@ -56,7 +55,7 @@ ActiveRecord::Schema.define(version: 20150930002126) do
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
     t.integer  "superior_public_agency_id", limit: 4
-    t.string   "name",                      limit: 255
+    t.string   "name",                      limit: 255''
     t.integer  "views_amount",              limit: 4
   end
 
