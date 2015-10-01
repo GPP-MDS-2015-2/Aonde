2a535bd6a2da83c8d013adee8b360dc933bd9527
Marcelo Ferreira <marcelohpf@gmail.com>
2015-09-30 22:06:54 -0300

    Change the form to share in the facebook

--- Aonde/app/helpers/public_agency_helper.rb
+++ Aonde/app/helpers/public_agency_helper.rb
@@ -3,5 +3,15 @@ module PublicAgencyHelper
 	  	total_expense = Expense.where(public_agency_id: id_pub_agency).sum(:value)
 	  	return total_expense
 	end
+	def facebook_share_amount(url_shared)
+		#data = Net::HTTP.get(URI.parse("http://graph.facebook.com/?ids=#{URI.escape(url_shared)}"))
+	    data = {'url_shared' => "asdf", 'shares' => 1}
+	    #data = JSON.parse(data)	
+    	if data != nil
+    	#	return data[url_shared]['shares']	 	
+    	 	return data['shares']
+    	else
+    		return 0
+    	end
+	end
 end	
-
