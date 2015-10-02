5e8573bd4ad61e3c8996b7b96074e8faef1b1ac7
Gabriel Silva <gabriel93.silva@gmail.com>
2015-10-02 02:06:13 -0300

    Generating metrics for release 1

--- Aonde/metrics/output/flog.js
+++ Aonde/metrics/output/flog.js
@@ -1,3 +1,3 @@
         var graph_title = 'Flog: code complexity';
-        var graph_series = [{name: 'average', data: [3.764377690035132]},{name: 'top 5% average', data: [9.95238664843765]}];
-        var graph_labels = {"0":"9/22"};
+        var graph_series = [{name: 'average', data: [3.764377690035132,8.638157362430487]},{name: 'top 5% average', data: [9.95238664843765,35.38054150409563]}];
+        var graph_labels = {"0":"9/22","1":"10/2"};
--- Aonde/metrics/output/rails_best_practices.js
+++ Aonde/metrics/output/rails_best_practices.js
@@ -1,3 +1,3 @@
         var graph_title = 'Rails Best Practices: design problems';
-        var graph_series = [{name: 'rails_best_practices', data: [11]}];
-        var graph_labels = {"0":"9/22"};
+        var graph_series = [{name: 'rails_best_practices', data: [11,21]}];
+        var graph_labels = {"0":"9/22","1":"10/2"};
