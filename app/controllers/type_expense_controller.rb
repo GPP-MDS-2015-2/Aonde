4fa86961c6e0daa4710df8384b42e8ed5aeed67f
Artur Bersan de Faria <artur_bersan@hotmail.com>
2015-10-16 17:55:16 -0300

    Create method define_color
     Signed-off-by Marcelo Ferreira <marcelohpf@gmail.com>

--- app/controllers/type_expense_controller.rb
+++ app/controllers/type_expense_controller.rb
@@ -8,21 +8,24 @@ class TypeExpenseController < ApplicationController
 	def get_expense_by_type(id_public_agency)
 			list_type_expenses = []			
 			types_expense = TypeExpense.all
+			total_expense = 0
 			types_expense.each do |type|
 				total_expense_by_type = find_exepenses(id_public_agency,type.id)
 				dictionary_expense = create_dictionary(total_expense_by_type,type)
 				if not dictionary_expense.empty?
+					total_expense += total_expense_by_type
 					list_type_expenses << dictionary_expense
 				else
 					# Do nothing
 				end
 			end
+			define_color(total_expense,list_type_expenses)
 			return list_type_expenses.to_json
 	end
 
 	def create_dictionary(value_expense, type_expense)
 		expense_dictionary = {}
-		if value_expense != 0
+		if value_expense != 0 
 			expense_dictionary = {name: type_expense.description,value: value_expense,colorValue: 10}
 		else
 			# Do nothing
@@ -34,5 +37,16 @@ class TypeExpenseController < ApplicationController
  		expense_value = Expense.where(public_agency_id: id_public_agency,
 			 							   type_expense_id: id_type_expense).sum(:value)
  		return expense_value
+	end
+	def define_color(total_expense,list_type_expenses)	
+		
+		expense_per_cent = 0
+
+		list_type_expenses.each do |expense|
+			expense_per_cent = expense[:value]*100/total_expense
+			expense_per_cent.to_i
+			expense[:colorValue] = expense_per_cent
+
+		end		
 	end
 end
