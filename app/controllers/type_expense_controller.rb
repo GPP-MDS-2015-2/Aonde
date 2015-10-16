ef11fad31820b918c882006b929b2b9b35c4f05c
Marcelo Ferreira <marcelohpf@gmail.com>
2015-10-16 13:22:05 -0300

    Add methods to TypeExpense
    Signed-off-by Artur Bersan de Faria <artur_bersan@hotmail.com>

--- app/controllers/type_expense_controller.rb
+++ app/controllers/type_expense_controller.rb
@@ -9,10 +9,30 @@ class TypeExpenseController < ApplicationController
 			list_type_expenses = []			
 			types_expense = TypeExpense.all
 			types_expense.each do |type|
-				total_expense_by_type = Expense.where(public_agency_id: id_public_agency,
-			 							   type_expense_id: id_type_expense).sum(:value)
-				list_type_expenses <<{type.description:total_expense_by_type}
+				total_expense_by_type = find_exepenses(id_public_agency,type.id)
+				dictionary_expense = create_dictionary(total_expense_by_type,type)
+				if not dictionary_expense.empty?
+					list_type_expenses << dictionary_expense
+				else
+					# Do nothing
+				end
 			end
 			return list_type_expenses.to_json
+	end
+
+	def create_dictionary(value_expense, type_expense)
+		expense_dictionary = {}
+		if value_expense != 0
+			expense_dictionary = {name: type_expense.description,value: value_expense,colorValue: 10}
+		else
+			# Do nothing
+		end
+		return expense_dictionary
+	end
+
+	def find_exepenses(id_public_agency = 0, id_type_expense = 0)
+ 		expense_value = Expense.where(public_agency_id: id_public_agency,
+			 							   type_expense_id: id_type_expense).sum(:value)
+ 		return expense_value
 	end
 end
