9c8382c76932d214c8f1b4d383dfd437f674e00b
Marcelo Ferreira <marcelohpf@gmail.com>
2015-10-18 21:41:10 -0200

    Add remove type from chart
    Signed-off-by: Artur Bersan de Faria <artur_bersan@hotmail.com>

--- app/controllers/type_expense_controller.rb
+++ app/controllers/type_expense_controller.rb
@@ -1,4 +1,5 @@
 class TypeExpenseController < ApplicationController
+	
 	def show
 
 		find_agencies(params[:id])
@@ -8,19 +9,18 @@ class TypeExpenseController < ApplicationController
 	end
 
 	def filter_chart
-	
+
 		find_agencies(params[:id])
 		@data_type_expense = get_expense_by_type(@public_agency.id,params[:year],params[:month])
-
+		
 		#puts "#{@data_type_expense.empty?} #{params[:year]} #{params[:month]}"
 		
 		@expense_type_find = 0
-		if not @data_type_expense.empty?
-			@expense_type_find = 1 
-			# no mensage!
+		if not is_empty_filter(@data_type_expense)
+			@expense_type_find = 1
+			# Nothing to do
 		else
-			flash[:error] = "Nenhuma despesa encontrada no periodo desejado! Veja o gráfico total:"
-			@data_type_expense = get_expense_by_type(@public_agency.id)			
+			@data_type_expense = get_expense_by_type(@public_agency.id)
 		end
 
 		@data_type_expense = @data_type_expense.to_json
@@ -34,7 +34,7 @@ class TypeExpenseController < ApplicationController
 		types_expense = TypeExpense.all
 		total_expense = 0
 		types_expense.each do |type|
-			total_expense_by_type = find_exepenses(id_public_agency,type.id,year,month)
+			total_expense_by_type = find_expenses(id_public_agency,type.id,year,month)
 			dictionary_expense = create_dictionary(total_expense_by_type,type)
 			if not dictionary_expense.empty?
 				total_expense += total_expense_by_type
@@ -47,28 +47,28 @@ class TypeExpenseController < ApplicationController
 		return list_type_expenses
 	end
 
-	def create_dictionary(value_expense, type_expense)
-		expense_dictionary = {}
-		if value_expense != 0 
-			expense_dictionary = {name: type_expense.description,value: value_expense,colorValue: 10}
-		else
-			# Do nothing
-		end
-		return expense_dictionary
-	end
-
-	def find_exepenses(id_public_agency = 0, id_type_expense = 0,year = "Todos",month="Todos")
+	def find_expenses(id_public_agency = -1, id_type_expense = -1,year = "Todos",month="Todos")
  		expenses_find = Expense.where(public_agency_id: id_public_agency,
 			 							   type_expense_id: id_type_expense)
  		expense_value = 0
  		expenses_find.each do |expense|
  			#puts "#{year} #{month} == #{"Todos"} #{"Todos" == month} #{expense.payment_date} "
  			if is_date_select(year,month,expense)
-
  				expense_value += expense.value
  			end
  		end
  		return expense_value
+	end
+
+	def create_dictionary(value_expense, type_expense)
+		expense_dictionary = {}
+		if value_expense > 0 
+			expense_dictionary = {name: type_expense.description,
+								value: value_expense,colorValue: 0}
+		else
+			# Do nothing
+		end
+		return expense_dictionary
 	end
 
 	def is_date_select(year = "Todos", month = "Todos", expense = nil)
@@ -86,35 +86,26 @@ class TypeExpenseController < ApplicationController
 			# date_select true Will permit sum all expenses
 		end
 
-		if year == nil or month == nil
-			date_select = true
-		elsif year.empty? or month.empty?
-			date_select = true
-		end
-
 		return date_select
 	end
 
-	def is_empty_filter(list_type_expenses)
+	def is_empty_filter(list_type_expenses=[])
+		
 		empty_filter = list_type_expenses.empty?
+		
 		if not empty_filter
-			@expense_type_find = true
-			# Find some expenes
+			# Do nothing
 		else
-			@expense_type_find = false
-			flash[:error] = "Não encontrol nenhuma despesa neste ano/mes:"
+			flash[:error] = "Nenhuma despesa encontrada no periodo desejado! Veja o gráfico total:"
 		end
 		return empty_filter
 	end
 
 	def define_color(total_expense,list_type_expenses)	
 		
-		expense_per_cent = 0
 		list_type_expenses.each do |expense|
 			expense_per_cent = expense[:value]*100/total_expense
-			expense_per_cent.to_i
-			expense[:colorValue] = expense_per_cent
-
+			expense[:colorValue] = expense_per_cent.to_i
 		end		
 	end
-end
+end	
