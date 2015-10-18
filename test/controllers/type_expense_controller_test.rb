dfcad5f5511e35275068b0db2228c7bd39db5f79
Marcelo Ferreira <marcelohpf@gmail.com>
2015-10-17 22:50:10 -0300

    Create test to method find_exepenses
    Signed-off-by: Artur Bersan de Faria <artur_bersan@hotmail.com>

--- test/controllers/type_expense_controller_test.rb
+++ test/controllers/type_expense_controller_test.rb
@@ -3,6 +3,64 @@ require 'test_helper'
 
 class TypeExpenseControllerTest < ActionController::TestCase
 		#assert_routing({ path: 'public_agency/:id/expenese_type', method: :get },{ controller: 'type_expense', action: 'get_expense_by_type' })
+	
+
+	test "The method find_exepenses" do
+
+		public_agency_default = @controller.find_exepenses
+		expense_value_init = 0
+		assert_equal(expense_value_init,public_agency_default)
+		
+		create_entities
+		
+		id_public_agency = 1
+		type_expense_one = 1
+		public_agency = @controller.find_exepenses(id_public_agency,type_expense_one)
+		assert_equal(public_agency,300)
+
+
+		type_expense_two = 2
+		public_agency_false = @controller.find_exepenses(id_public_agency,type_expense_two)
+		assert_equal(public_agency_false,0)				
+
+	end
+
+	test "The method create_dictionary" do
+		type_expense_equal = TypeExpense.new(description: "teste description")
+		value_expense_positive = 100
+		positive_expense = @controller.create_dictionary(value_expense_positive,type_expense_equal)
+		expect_dictionary_equal = {name: "teste description",value: 100,	colorValue: 0}
+		
+		assert_equal(expect_dictionary_equal, positive_expense)
+		
+		expect_dictionary_different = {name: "teste description",value: 101,	colorValue: 0}
+		assert_not_equal(expect_dictionary_different, positive_expense)
+
+		value_expense_negative = -1
+		negative_expense = @controller.create_dictionary(value_expense_negative,type_expense_equal)
+		assert (negative_expense.empty?)
+
+		value_expense_zero = 0
+
+		zero_expense = @controller.create_dictionary(value_expense_negative,type_expense_equal)
+		assert (zero_expense.empty?)
+	end
+
+	def create_entities
+		PublicAgency.create(id: 1,views_amount: 0,name: "valid Agency")
+		PublicAgency.create(id: 2,views_amount: 0,name: "valid Agency")
+
+		TypeExpense.create(id: 1, description: "Type expense valid")
+		TypeExpense.create(id: 2, description: "Type expense invalid")
+
+		Expense.create(document_number: "0000",payment_date: Date.new(2010,1,1),
+							public_agency_id: 2,type_expense_id: 2,value: 100)
+		for i in 1..3
+			Expense.create(document_number: "0000",payment_date: Date.new(2013+i,i,1),
+							public_agency_id: 1,type_expense_id: 1,value: 100)
+		end
+	end
+	
   test "The calculo of the color based in the porcent in method define_color" do
   	total_expense = 100
 
@@ -57,8 +115,7 @@ class TypeExpenseControllerTest < ActionController::TestCase
 
 	end  
 
-	test "Verify if is_date_select is true" do
-		
+	test "Verify if is_date_select is true" do	
 		
 		default_params = @controller.is_date_select
 		assert(default_params)
@@ -82,26 +139,11 @@ class TypeExpenseControllerTest < ActionController::TestCase
 		assert(params_validate)
 
 
-	end	
-
-	test "The method create_dictionary" do
-		type_expense_equal = TypeExpense.new(description: "teste description")
-		value_expense_positive = 100
-		positive_expense = @controller.create_dictionary(value_expense_positive,type_expense_equal)
-		expect_dictionary_equal = {name: "teste description",value: 100,	colorValue: 0}
-		
-		assert_equal(expect_dictionary_equal, positive_expense)
-		
-		expect_dictionary_different = {name: "teste description",value: 101,	colorValue: 0}
-		assert_not_equal(expect_dictionary_different, positive_expense)
-
-		value_expense_negative = -1
-		negative_expense = @controller.create_dictionary(value_expense_negative,type_expense_equal)
-		assert (negative_expense.empty?)
-
-		value_expense_zero = 0
-
-		zero_expense = @controller.create_dictionary(value_expense_negative,type_expense_equal)
-		assert (zero_expense.empty?)
 	end
+
+
+
+	
+
+
 end
