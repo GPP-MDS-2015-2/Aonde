ad3086fbc6711e90edbbbbb0f60cee1462f0ca7d
matheus miranda <matheus-m-ml@hotmal.com>
2015-10-19 11:48:19 -0200

    Test method sort_by_expense on company_controller
    Signed-off-by: jpbusche <joaopaulo.busche@gmail.com>

--- test/controllers/company_controller_test.rb
+++ test/controllers/company_controller_test.rb
@@ -1,14 +1,13 @@
 require 'test_helper'
 
 class CompanyControllerTest < ActionController::TestCase
+
 	test "Route to method show and the result of the request" do
 
 		generate_public_agency
 		assert_routing 'public_agency/1/company', { :controller => "company", :action => "show", :id => "1" }
 		get :show, id: 1
 		assert_response :success
-		assert_not_nil assigns(:array_companies_expenses)
-		assert assigns(:array_companies_expenses)
 
 	end
 
@@ -17,6 +16,15 @@ class CompanyControllerTest < ActionController::TestCase
 		PublicAgency.create(id: 1,views_amount: 0,name: "valid Agency")
 		
 	end	
+
+	test "should return a ordered hash" do
+
+		hash = {"key1"=>12,"key2"=>8,"key3"=>10}
+		hash = @controller.sort_by_expense(hash)
+		ordered_hash = {"key2"=>8,"key3"=>10,"key1"=>12}.to_a
+		assert_equal(hash,ordered_hash)
+
+	end
 
 end
 
