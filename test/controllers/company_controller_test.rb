d1cd282d46822fe1864a3be867476154c969b8a7
matheus miranda <matheus-m-ml@hotmal.com>
2015-10-19 11:50:12 -0200

    Test method test_add_expense on company_controller
    Signed-off-by: jpbusche <joaopaulo.busche@gmail.com>

--- test/controllers/company_controller_test.rb
+++ test/controllers/company_controller_test.rb
@@ -26,5 +26,29 @@ class CompanyControllerTest < ActionController::TestCase
 
 	end
 
+	test "should validate an array of companies" do
+
+		company = Company.new(id: 1000,name: "company 1")
+		company1 = Company.new(id: 1001,name: "company 2")
+		
+		company_multiples = [company,company1]
+
+		expense = Expense.new(value: 100,payment_date: Date.new, document_number: '0000')
+		hash_companies_multiple = {}
+		@controller.test_add_expense(company_multiples,expense,hash_companies_multiple)
+		assert hash_companies_multiple.empty?
+		
+		company_empty = []
+		hash_companies_empty = {}
+		@controller.test_add_expense(company_empty,expense,hash_companies_empty)
+		assert hash_companies_empty.empty?
+
+		company_single = [company]
+		hash_companies_valid = {}
+		@controller.test_add_expense(company_single,expense,hash_companies_valid)
+		assert_not hash_companies_valid.empty?		
+
+	end
+
 end
 
