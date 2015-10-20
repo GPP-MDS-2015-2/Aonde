c9380b45221a6672f608f0fd05af7f58c8ceb6ac
ArthurTemporim <arthurrt@gmail.com>
2015-10-20 16:09:29 -0200

    Add methods to test controller of programs
    Signed-off-by: eduardonunes2525 <eduardonunes25@gmail.com>
    Signed-off-by: ArthurTemporim <arthurrt@gmail.com>

--- app/controllers/program_controller.rb
+++ app/controllers/program_controller.rb
@@ -1,13 +1,15 @@
 class ProgramController < ApplicationController
 
 	def show
-		find_expenses
-	end	
+		find_public_agency
+		find_expenses(@public_agency.id)
+	end
+
 	def find_public_agency
 		@public_agency = PublicAgency.find(params[:id])	
 	end
 
-	def find_expenses
+	def find_expenses(public_agency_id)
 		find_public_agency
 		expenses_public_agency = Expense.where(public_agency_id: @public_agency.id)
 		@all_programs = find_program(expenses_public_agency).to_a
@@ -15,24 +17,29 @@ class ProgramController < ApplicationController
 	end
 
 	def find_program(expenses_public_agency)
-
 		programs_expense = {}
 
 		expenses_public_agency.each do |expense|
 
 			program = Program.where(id: expense.program_id)
-			#
-			if not program.empty? and program.length == 1
-				
-				program = program[0]
-				if not programs_expense [program.name]
-	          		programs_expense [program.name] = expense.value
-	        	else
-	        		programs_expense[program.name] += expense.value
-	        	end
-
-	        end
 		end
 		return programs_expense
+	end
+
+	def sum_expense_program (program, expense, programs_expense)
+		if not program.empty? and program.length == 1
+			program	= program[0]
+			add_expense_program(program, expense, programs_expense)
+		else
+			#do nothing
+	    end
+	end
+
+	def add_expense_program (program, expense, programs_expense)
+		if not programs_expense [program.name]
+	    	programs_expense [program.name] = expense.value
+	    else
+	    	programs_expense[program.name] += expense.value
+	    end	
 	end
 end
