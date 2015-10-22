93b3daca7f2e03470c6e19ff872c22bb8d2729b7
Marcelo Ferreira <marcelohpf@gmail.com>
2015-10-22 20:58:22 -0200

    Add method get_api_data and get_url
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>
    Signed-off-by: ArthurTemporim <arthurrt@gmail.com>

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -15,4 +15,28 @@ class Budget < ApplicationController
     return url_query
   end
 
+  def get_url(year,code_public_agency)
+    year = year.to_s
+    code_public_agency.to_s
+    code_public_agency = code_public_agency.to_s
+
+    begin_url = "http://orcamento.dados.gov.br/sparql/?default-graph-uri=&query="
+
+    end_url = "&debug=on&timeout=&format=application%2Fsparql-results%2Bjson&save=display&fname="
+
+    url_query = generate_query(year,code_public_agency)
+
+    url = begin_url+url_query+end_url
+
+    puts "\n\n\n\n#{url}\n\n\n"
+
+    return url
+  end
+
+  def get_api_data(year,code_public_agency)
+    data = Net::HTTP.get(URI.parse(get_url(year,code_public_agency)))
+    puts "#{data}"
+    return data
+  end
+
 end
