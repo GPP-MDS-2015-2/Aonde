42b50ef2b0f0f155e7db1dece7dd55ba604869e9
Artur Bersan de Faria <artur_bersan@hotmail.com>
2015-10-23 16:55:48 -0200

    "Add methods that return a hash with function and value"
    Signed-off-by: Matheus Miranda <matheus-m-ml@hotmal.com>
    Signed-off-by: Artur Bersan de Faria <artur_bersan@hotmail.com>

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,4 +3,5 @@ class Expense < ActiveRecord::Base
   belongs_to :public_agency
   validates :document_number, uniqueness: true,on: :create,presence: true
   validates :payment_date, presence: true,on: :create
+  has_one :function
 end
