ce88bb35efe118480176a30aec1b2b45f9773b43
Marcelo Ferreira <marcelohpf@gmail.com>
2015-10-23 16:44:47 -0200

    Fix a validate in the model expense and the test in the company_controller
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>
    Signed-off-by: ArthurTemporim <arthurrt@gmail.com>

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,6 @@
 class Expense < ActiveRecord::Base
   belongs_to :program 
   belongs_to :public_agency
-  validates :document_number, uniqueness: true,on: :create,presence: true
+  validates :document_number,on: :create,presence: true
   validates :payment_date, presence: true,on: :create
 end
