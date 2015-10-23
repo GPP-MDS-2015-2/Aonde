33ab90b6bc6381a1ece022c3a5cf7a6c2b20e45d
matheus miranda <matheus-m-ml@hotmal.com>
2015-10-23 10:25:35 -0200

    Add method to group expenses and sum value
    Signed-off-by Artur Bersan de Faria <artur_bersan@hotmail.com>

---
+++ app/controllers/function_controller.rb
@@ -0,0 +1,9 @@
+class FunctionController < ApplicationController
+
+	def insert_expenses_functions
+
+		expense = Function.get_expenses_by_function
+			
+	end
+
+end	
--- app/models/function.rb
+++ app/models/function.rb
@@ -1,2 +1,9 @@
 class Function < ActiveRecord::Base
+
+	def get_expenses_by_function
+
+  		Expense.select("sum(value) as sumValue,function_id").group("function_id").to_json
+
+	end
+
 end
