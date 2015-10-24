17153eebdc9bf8eff1977d61aa8c3cdfc98a5c2d
ArthurTemporim <arthurrtl@gmail.com>
2015-10-24 19:38:30 -0200

    Add method get_all_budget
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -1,16 +1,38 @@
 class BudgetController < ApplicationController
-  def get_api_data(year, code_public_agency)
+  def get_api_data(year, public_agency_id)
     budget_years = []
     begin
-      data = Net::HTTP.get(URI.parse(get_url(year, code_public_agency)))
-      budget_hash = parse_json_to_hash(data)
-      value_budget = get_value_budget(budget_hash)
-      budget_years << value_budget
+      budget_years = get_all_budget(public_agency_id)
+      puts budget_years
     rescue Exception => error
-      puts '#{error}'
+      budget_years = error
+      puts error
     end
 
     return budget_years
+  end
+
+  def get_all_budget(public_agency_id)
+    budget_years = []
+    for year in 2011..2015
+      data_api = {}
+      begin
+        data_api = obtain_api_data(year, public_agency_id)
+      rescue
+        raise "Não foi possível conectar a API"
+      end
+      budget_hash = parse_json_to_hash(data_api)
+      value_budget = get_value_budget(budget_hash)
+      budget_years << value_budget
+    end
+    return budget_years
+  end
+
+  def obtain_api_data(year, public_agency_id)
+    url_query = get_url(year, public_agency_id)
+    uri_query = URI.parse(url_query)
+    data_api = Net::HTTP.get(uri_query)
+    return data_api
   end
 
   def get_value_budget(budget_hash)
@@ -21,48 +43,47 @@ class BudgetController < ApplicationController
     if !sum_total_budget.empty?
       value = sum_total_budget['somaProjetoLei']['value']
     else
-      fail 'Não foi possível obter o valor do orçamento pela api'
+      raise "Não foi possível obter o valor do orçamento pela api"
     end
 
     return value
   end
 
-  def parse_json_to_hash(data)
+  def parse_json_to_hash(data_api)
     budget_year = {}
     begin
-      budget_year = JSON.parse(data)
+      budget_year = JSON.parse(data_api)
     rescue
       raise 'Não foi possivel conventer os dados da API do orçamento'
     end
     return budget_year
   end
 
-  def get_url(year, code_public_agency)
+  def get_url(year, public_agency_id)
     begin_url = 'http://orcamento.dados.gov.br/sparql/?default-graph-uri=&query='
 
     end_url = '&debug=on&timeout=&format=application%2Fsparql-results%2Bjson'\
     '&save=display&fname='
 
     year = year.to_s
-    code_public_agency.to_s
-    code_public_agency = code_public_agency.to_s
+    public_agency_id = public_agency_id.to_s
 
-    url_query = generate_query(year, code_public_agency)
+    url_query = generate_query(year, public_agency_id)
 
     url = begin_url + url_query + end_url
 
-    puts '\n\n\n\n#{url}\n\n\n'
+    puts "\n\n\n\n#{url}\n\n\n"
 
     return url
   end
 
-  def generate_query(year, code_public_agency)
+  def generate_query(year, public_agency_id)
     query = 'SELECT (SUM(?ProjetoLei) AS ?somaProjetoLei) WHERE {'\
     '?itemBlankNode loa:temExercicio ?exercicioURI . '\
     '?exercicioURI loa:identificador ' + year + ' . '\
     '?itemBlankNode loa:temUnidadeOrcamentaria ?uoURI . '\
     '?uoURI loa:temOrgao ?orgaoURI . '\
-    '?orgaoURI loa:codigo "' + code_public_agency + '" . '\
+    '?orgaoURI loa:codigo "' + public_agency_id + '" . '\
     '?itemBlankNode loa:valorProjetoLei ?ProjetoLei . }'
 
     url_query = URI.encode(query)
