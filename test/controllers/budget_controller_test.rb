f2cc6dfb021fce236367d1a3a79823693aae23a8
ArthurTemporim <arthurrtl@gmail.com>
2015-10-24 21:49:13 -0200

    Add test to budget controller method get_url
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- test/controllers/budget_controller_test.rb
+++ test/controllers/budget_controller_test.rb
@@ -18,6 +18,39 @@ class BudgetControllerTest < ActionController::TestCase
 
     assert_not_equal(query_not_encode, query_encode)
   end
+
+   def get_url(year, public_agency_id)
+    begin_url = 'http://orcamento.dados.gov.br/sparql/?default-graph-uri=&query='
+
+    end_url = '&debug=on&timeout=&format=application%2Fsparql-results%2Bjson'\
+    '&save=display&fname='
+
+    year = year.to_s
+    public_agency_id = public_agency_id.to_s
+
+    url_query = generate_query(year, public_agency_id)
+
+    url = begin_url + url_query + end_url
+
+    puts "\n\n\n\n#{url}\n\n\n"
+
+    return url
+  end
+
+  test "generate get correct url" do
+    correct_url = 'http://orcamento.dados.gov.br/sparql/?default-graph-uri=&query='\
+    + URI.encode(create_query("2015","1")) +
+    '&debug=on&timeout=&format=application%2Fsparql-results%2Bjson'\
+    '&save=display&fname='
+    method_url = @controller.get_url("2015","1")
+    assert_equal(correct_url, method_url)
+  end
+
+  test "generate get nil url" do
+    method_url = @controller.get_url("2015","1")
+    assert_not_nil(method_url)
+  end
+
   private
     def create_query(year,public_agency_id)
       test_query = 'SELECT (SUM(?ProjetoLei) AS ?somaProjetoLei) WHERE {'\
