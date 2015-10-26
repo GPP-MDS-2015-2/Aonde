f946c472c35a7929e2c6322247dd1f65086a39e3
matheus miranda <matheus-m-ml@hotmail.com>
2015-10-25 22:56:34 -0200

    Implement chart filter
    Signed-off-by: Artur Bersan de Faria <artur_bersan@hotmail.com>
    Signed-off-by: matheus miranda <matheus-m-ml@hotmail.com>

--- app/models/function.rb
+++ app/models/function.rb
@@ -2,10 +2,12 @@ class Function < ActiveRecord::Base
 
 	has_many :expense
 
-	def get_expenses_by_function
+	def get_expenses_by_function(year = 2015,month = 1)
 
-  		Function.joins(:expense).select("sum(expenses.value) as sumValue,functions.description")
-  		         						.group("functions.description").to_json
+		start = Date.new(year,1,1)
+		end_of = Date.new(year,12,28)
+
+  		Function.joins(:expense).where("DATE(payment_date) BETWEEN ? AND ?", start, end_of).select("YEAR(payment_date) as date_test,sum(expenses.value) as sumValue,functions.description").group("YEAR(payment_date),functions.description").to_json
 
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   #Routes of Programs
   get "public_agency/:id/programs" => "program#show"
   get "/functions" => "function#show"
+  get "/functions/filter" => "function#show"
+    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
