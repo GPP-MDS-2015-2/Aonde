8ce5ccfc785aad45ffe0482c1e619ff7901a65a3
Artur Bersan de Faria <artur_bersan@hotmail.com>
2015-10-27 08:28:41 -0200

    Fix errors in tests
    Signed-off-by: matheus miranda <matheus-m-ml@hotmail.com>
    Signed-off-by: Artur Bersan de Faria <artur_bersan@hotmail.com>

--- test/controllers/function_controller_test.rb
+++ test/controllers/function_controller_test.rb
@@ -4,7 +4,7 @@ class FunctionControllerTest < ActionController::TestCase
 	
 	test "Should validate atributtes" do
 
-		@controller.insert_expenses_functions
+		@controller.insert_expenses_functions(2015,2015,1,12,1,31)
 		assert_nil(@function)
 		
 	end
@@ -26,6 +26,16 @@ class FunctionControllerTest < ActionController::TestCase
 
 	end
 
+
+	test "Should not convert to a hash" do
+
+		hash = [{"key1"=>1},{"key2"=>2}].to_json
+		expected_hash = {{"key1"=>1}=>{"key2"=>2}}
+		returned_hash = @controller.convert_to_a_hash(hash)
+		assert_not_equal(expected_hash,returned_hash)
+
+	end
+
 	test "Should filter datas" do
 
 		hash_json = [{"id"=>nil,"description"=>"Saúde","sumValue"=>2}]
@@ -35,7 +45,7 @@ class FunctionControllerTest < ActionController::TestCase
 
 	end
 
-	test "Should return the last day of month" do
+	test "Should return the last day of month,first case" do
 
 		month_number = 1
 		last_day = @controller.find_month_limit(month_number)
@@ -44,7 +54,7 @@ class FunctionControllerTest < ActionController::TestCase
 
 	end
 
-	test "should return the last day of month" do
+	test "should return the last day of month,second case" do
 
 		month_number = 4
 		last_day = @controller.find_month_limit(month_number)
@@ -54,7 +64,7 @@ class FunctionControllerTest < ActionController::TestCase
 	end
 
 
-	test "should return the last day of month" do
+	test "should return the last day of month,third case" do
 
 		month_number = 2
 		last_day = @controller.find_month_limit(month_number)
