4358ac425049470d3b90c08981896fe0002a5871
Eduardo_Nunes <eduardonunes2525@gmail.com>
2015-10-29 18:54:51 -0200

    Add method create_nodes and modify tests
    Signed-off-by: Eduardo_Nunes <eduardonunes2525@gmail.com>
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- app/controllers/program_controller.rb
+++ app/controllers/program_controller.rb
@@ -1,68 +1,74 @@
 class ProgramController < ApplicationController
 
-    def show
-   	 find_agencies(params[:id])
-   	 @all_programs = find_expenses(@public_agency.id)
-   	 @all_programs.to_json
+  def show
+   find_agencies(params[:id])
+   @all_programs = find_expenses(@public_agency.id)
+   @all_programs.to_json
+  end
+
+  def find_expenses(public_agency_id)
+   expenses_public_agency = Expense.where(public_agency_id: public_agency_id)
+   list_expenses =  find_program(expenses_public_agency).to_a
+   return list_expenses
+  end
+
+
+  def find_program(find_expenses_public_agency)
+    programs_expense = {}
+
+    find_expenses_public_agency.each do |expense|
+                        #PROBLEMA AQUI
+      program = Program.where(id: expense.program_id)
+      sum_expense_program(program,expense,programs_expense)
+      #puts "#{programs_expense}"
+    end
+    return programs_expense
+  end
+
+  def sum_expense_program (program, expense, programs_expense)
+    if not program.empty? and program.length == 1
+      program = program[0]
+      add_expense_program(program, expense, programs_expense)
+    else
+      #do nothing
+      end
+  end
+
+  def add_expense_program (program, expense, programs_expense)
+    if not programs_expense [program.name]
+        programs_expense [program.name] = expense.value
+    else
+      programs_expense [program.name] += expense.value
+    end 
+  end
+###########################################################3  
+  def create_nodes(program_id, program_related,field_entity)
+    begin
+      agency_list = find_entities(program_id,field_entity)
+      agency_list.each do |agency|
+        add_node(agency,program_related)
+        add_vertice(program_related)
+      end
+    rescue Exception => e
+      puts "\n#{e}"
+    end
+  end
+  def find_entities(program_id, field_entity)
+    entities = Expense.select("DISTINCT("+field_entity+")").where(program_id: program_id)
+    
+    if !entities.nil? && !entities.empty?
+      #do nothing
+    else
+      raise "Entities not found! \n #{program_id} with field #{field_entity}"
     end
 
-    def find_expenses(public_agency_id)
-   	 expenses_public_agency = Expense.where(public_agency_id: public_agency_id)
-   	 list_expenses =  find_program(expenses_public_agency).to_a
-   	 return list_expenses
-    end
-
-
-	def find_program(find_expenses_public_agency)
-		programs_expense = {}
-
-		find_expenses_public_agency.each do |expense|
-												#PROBLEMA AQUI
-			program = Program.where(id: expense.program_id)
-			sum_expense_program(program,expense,programs_expense)
-			#puts "#{programs_expense}"
-		end
-		return programs_expense
-	end
-
-	def sum_expense_program (program, expense, programs_expense)
-		if not program.empty? and program.length == 1
-			program	= program[0]
-			add_expense_program(program, expense, programs_expense)
-		else
-			#do nothing
-	    end
-	end
-
-	def add_expense_program (program, expense, programs_expense)
-		if not programs_expense [program.name]
-	    	programs_expense [program.name] = expense.value
-	    else
-	    	programs_expense [program.name] += expense.value
-	    end	
-	end
-#####################################################################################################	
-	def create_nodes(program_id)
-		array_validation = [[{"id" => 1,"label" => "Programa1"},{"id"=>2,"label"=>
-		 "PublicAgency1"},{"id" => 3,"label" => "Company1"}],[{"from"=>1,"to"=>2},{"from"=>1,"to"=>3}]]
-		if program_id != nil
-			#Executa a parada 
-			return array_validation	
-		else
-			return [[],[]] 
-		end
-	end
-	def find_entities(program_id, field_entity)
-		if field_entity == "public_agency_id"
-			return [Expense.new(public_agency_id: 12345)]
-		else
-			return []
-		end
-	end
-	def add_node(entity,data_program)
-		data_program[0] <<"muahahah"
-	end
-	def add_vertice(data_program)
-		data_program[1] << "teste"
-	end
+    return entities 
+  end
+  
+  def add_node(entity,data_program)
+    data_program[0] <<"muahahah"
+  end
+  def add_vertice(data_program)
+    data_program[1] << "teste"
+  end
 end
