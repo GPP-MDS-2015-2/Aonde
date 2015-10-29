539745fd07fe6f9250129070e1fa9abb13ee911c
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2015-10-29 00:07:11 -0200

    Add link between travis_codeclimate

--- db/migrate/20151023125024_add_references_on_budgets.rb
+++
@@ -1,9 +0,0 @@
-class AddReferencesOnBudgets < ActiveRecord::Migration
-  def change
-  	change_table :budgets do |t|
-  		
-  		t.references :public_agency, index: true, foreign_key: true
-
-  	end
-  end
-end
