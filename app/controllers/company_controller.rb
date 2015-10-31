470301bf0953ac66b6046781821e0c960c85b230
ArthurTemporim <arthurrt@gmail.com>
2015-10-31 16:53:13 -0200

    Add method of generate nodes of graph
    Signed-off-by: matheus miranda <matheus-m-ml@hotmail.com>
    Signed-off-by: ArthurTemporim <arthurrt@gmail.com>

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -46,31 +46,81 @@ class CompanyController < ApplicationController
 
 
   def index
-    # 		expenses = Expense.where(company_id: params[:id])
-    # 		@array_programs_public_agencies = find_programs_and_public_agencies(expenses)=end
+    expenses = Expense.where(company_id: params[:id])
+    company_hiring_incidence = find_public_agencies(expenses)
+    company_name = get_name_company
+    get_name_hash(company_name, company_hiring_incidence)
   end
 
   def find_public_agencies(expenses)
     company_hiring_incidence  = {}
     expenses.each do |expense|
-      public_agency = PublicAgency.find(expenses.public_agency_id)     
+      public_agency = PublicAgency.find(expenses.public_agency_id)
       verify_insert(company_hiring_incidence,public_agency,counting)
     end
+    return company_hiring_incidence
   end
 
   def find_hiring_count(public_agency)
-    
     counting = Expense.where(public_agency_id: public_agency.id).count
-
   end
 
   def verify_insert(company_hiring_incidence,public_agency,counting)
-
     if ! company_hiring_incidence[public_agency.name]
       counting = find_hiring_count(public_agency)
-      company_hiring_incidence [public_agency.name] = counting
+      company_hiring_incidence[public_agency.name] = counting
     end
+  end
+
+  def get_name_company
+    company_name = Company.find(params[:id]).name
+    return company_name
+  end
+
+  def generate_position_x(node)
+    i = 100
+  end
+
+  def generate_position_y(node)
+    i = 80
+  end
+
+  def generate_company_node(company_name)
+    data_company = [
+      { "data" => { id => company_name }, position => { x => 100, y => 80 } },
+      { "data" => { id => 'Órgãos Públicos' } },{ "data" => { id => 'qtde Contratações' } },
+    ]
+  end
+
+  def generate_public_agency_node(company_hiring_incidence, node, data_company)
+    node = 1
+    company_hiring_incidence.each do |public_agency_name, hiring|
+      public_agency_name = public_agency_name.to_s
+      { "data" => { id => public_agency_name, parent => 'Órgãos Públicos' }, position => { x => generate_position_x(node), y => generate_position_y(node) } },
+    end
+
+    return node.push(hash)
 
   end
 
 end
+
+
+=begin
+elements: {
+    nodes: [
+      { "data": { id: 'Empresas' }, position: { x: 100, y: 80 } },
+      { "data": { id: 'Ministério2', parent: 'Órgãos Públicos' }, position: { x: 200, y: 80 } },
+      { "data": { id: 'qtde Contratações' } },
+      { "data": { id: '37', parent: 'qtde Contratações' }, position: { x: 350, y: 30 } },
+      { "data": { id: 'Órgãos Públicos' } },
+      { "data": { id: 'Ministério1', parent: 'Órgãos Públicos' }, position: { x: 200, y: 30 } },
+    ],
+    edges: [
+      { data: { id: 'Ministério2Empresas', source: 'Ministério2', target: 'Empresas' } },
+      { data: { id: '37Ministério1', source: '37', target: 'Ministério1' } },
+      { data: { id: 'Ministério1Empresas', source: 'Ministério1', target: 'Empresas' } }
+      
+    ]
+  }
+=end
