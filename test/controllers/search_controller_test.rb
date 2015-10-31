b8d15f71f581088ce0e38aaefd44d1b9dd29a745
Artur Bersan de Faria <artur_bersan@hotmail.com>
2015-10-31 20:27:44 -0200

    Create test to especif case of method search_entity
    Signed-off-by: jpbushe <joaopaulo.busche.@gmail.com>
    Signed-off-by: Artur Bersan de Faria <artur_bersan@hotmail.com>

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -2,62 +2,79 @@ require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
 
-	test "Should sum value expense of company" do
-		create_entities
-		id_company = 1
-		company_expense = @controller.expense_entities(:company_id,id_company)
-		assert_equal(company_expense, 400)
-		
-	end
+  test "Should sum value expense of company" do
+    create_entities
+    id_company = 1
+    company_expense = @controller.expense_entities(:company_id,id_company)
+    assert_equal(company_expense, 400)
+    
+  end
 
-	test "Should sum value expense of program" do
-		create_entities
-		id_program = 1
-		program_expense = @controller.expense_entities(:company_id,id_program)
-		assert_equal(program_expense, 400)
-	end
+  test "Should sum value expense of program" do
+    create_entities
+    id_program = 1
+    program_expense = @controller.expense_entities(:company_id,id_program)
+    assert_equal(program_expense, 400)
+  end
 
-	test "Should sum value expense of agency" do
-		create_entities
-		id_public_agency= 1
-		agency_expense = @controller.expense_entities(:company_id,id_public_agency)
-		assert_equal(agency_expense, 400)
-	end
+  test "Should return un especific entity" do
+    
+    entities_for_search
+    entity = @controller.search_entity(PublicAgency,:public_agency_id,"ministerio da educacao")
+    entity_compare = PublicAgency.find(2)
+    expected_result = [[entity_compare,0]]
+    assert_equal(expected_result,entity)
 
-	test "Routes to method idex" do
+  end
 
-		create_entities
-		
-		get :index, id: 1, search: "valid", entity: "Todos"
 
-		assert_response :success
+  test "Should sum value expense of agency" do
+    create_entities
+    id_public_agency= 1
+    agency_expense = @controller.expense_entities(:company_id,id_public_agency)
+    assert_equal(agency_expense, 400)
+  end
 
-		assert assigns(:entity)
-		assert assigns(:msg_error)
-		assert assigns(:public_agencies)
-		assert assigns(:total_expense_agency)
-		assert assigns(:companies)
-		assert assigns(:total_expense_company)
-		assert assigns(:programs)
-		assert assigns(:total_expense_program)
-		assert assigns(:results)
-		
-		
-	end
+  test "Routes to method idex" do
 
-	def create_entities
+    create_entities
+    
+    get :index, id: 1, search: "valid", entity: "Todos"
 
-		PublicAgency.create(id: 1,views_amount: 0,name: "valid Agency")
-		Program.create(id: 1,name: "valid Program")
-		Company.create(id: 1,name: "valid Company")
+    assert_response :success
 
-		for i in 1..4
+    assert assigns(:entity)
+    assert assigns(:msg_error)
+    assert assigns(:public_agencies)
+    assert assigns(:companies)
+    assert assigns(:programs)
+    assert assigns(:results)
+    
+    
+  end
 
-			Expense.create(document_number: "0000",payment_date: Date.new(2013,i,1),
-							public_agency_id: 1,program_id: 1,company_id: 1,value: 100)
+  def entities_for_search
 
-		end	
+    PublicAgency.create(id: 1,views_amount: 0,name:"ministerio da saude")
+    PublicAgency.create(id: 2,views_amount: 0,name:"ministerio da educacao")
+    PublicAgency.create(id: 3,views_amount: 0,name: "ministro John")
+    PublicAgency.create(id: 4,views_amount: 0,name: "agency")
 
-	end
+  end
+
+  def create_entities
+
+    PublicAgency.create(id: 1,views_amount: 0,name: "valid Agency")
+    Program.create(id: 1,name: "valid Program")
+    Company.create(id: 1,name: "valid Company")
+
+    for i in 1..4
+
+      Expense.create(document_number: "0000",payment_date: Date.new(2013,i,1),
+              public_agency_id: 1,program_id: 1,company_id: 1,value: 100)
+
+    end 
+
+  end
 
 end
