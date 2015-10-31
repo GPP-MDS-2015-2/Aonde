eedb34dc6bfa8a7ddbba8effca52a9b694f6b47a
Marcelo Ferreira <marcelohpf@gmail.com>
2015-10-31 16:00:57 -0200

    Add method to manage data and tests of routes
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>
    Signed-off-by: Eduardo_Nunes <eduardonunes2525@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   get "public_agency/:id/company", to: "company#show", as: "company"
   get 'public_agency/:id/budgets', to: "budget#show", as: "budget"
   get "public_agency/:id/programs", to: "program#show", as: "programs"
+  
+  get "/program/:id", to: "program#show_program", as: "program"
+
   get "public_agency/:id/filter_budget",to: "budget#filter_chart_budget", as: "filter_budget"
   #Routes of Programs
   get "/functions" => "function#show"
