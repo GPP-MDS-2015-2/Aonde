51688d2d2c51c6af27725bb96f4c7a37b0e282c3
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2015-11-03 23:06:23 -0200

    Generate metrics for Sprint 2

--- tmp/metric_fu/output/cane.js
+++ tmp/metric_fu/output/cane.js
@@ -1,3 +1,3 @@
         var graph_title = 'Cane: code quality threshold violations';
-        var graph_series = [{name: 'cane', data: [38,38,417,616]}];
-        var graph_labels = {"0":"9/22","1":"10/2","2":"10/22","3":"11/1"};
+        var graph_series = [{name: 'cane', data: [38,38,417,616,603]}];
+        var graph_labels = {"0":"9/22","1":"10/2","2":"10/22","3":"11/1","4":"11/3"};
