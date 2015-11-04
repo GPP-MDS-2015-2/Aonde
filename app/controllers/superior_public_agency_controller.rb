476c3db48abfad95786b1b2445945dbb68854359
João Paulo Busche <joaopaulo.busche@gmail.com>
2015-11-04 12:22:29 -0200

    Create route and web page
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>
    Signed-off-by: João Paulo Busche <joaopaulo.busche@gmail.com>

--- app/controllers/superior_public_agency_controller.rb
+++ app/controllers/superior_public_agency_controller.rb
@@ -1,4 +1,10 @@
 class SuperiorPublicAgencyController < ApplicationController
-	def index
+	def show
+		@superior_agency = SuperiorPublicAgency.find(params[:id])
 	end
+
+	def find_public_agencies(superior_id)
+		public_agencies = PublicAgency.where(superior_public_agency_id: superior_id)
+	end
+
 end
