9e6910217eb7f8a38edf07ba1edf3b5c35bcbc1c
Eduardo_Nunes <eduardonunes2525@gmail.com>
2015-11-10 03:22:21 -0200

    Add sidebar and new search field footer in another pages
    Signed-off-by: Eduardo_Nunes <eduardonunes2525@gmail.com>
    Signed-off-by: matheus miranda <matheus-m-ml@hotmail.com>

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -113,3 +113,13 @@ $(document).ready(function() {
     $('.parallax').parallax();
   });
 })(jQuery);
+
+(function($){
+  $(document).ready(function(){
+      $('.modal-trigger').leanModal();
+      $('.button-collapse').sideNav();
+      $('select').material_select();
+      $('.tooltipped').tooltip({delay: 50});
+      
+  });
+})(jQuery);
