2a58ebbbf0d273deb3789503845d48f905c8adae
Eduardo_Nunes <eduardonunes2525@gmail.com>
2015-11-20 00:14:11 -0200

    Change the css for layout
    Signed-off-by: Artur Bersan de Faria  <artur_bersan@hotmail.com>
    Signed-off-by: Eduardo_Nunes <eduardonunes2525@gmail.com>

--- app/helpers/public_agency_helper.rb
+++ app/helpers/public_agency_helper.rb
@@ -1,18 +1,18 @@
 module PublicAgencyHelper
 	
 	def facebook_share_amount(url_shared)
-		#url = URI.parse("http://graph.facebook.com/?ids=#{URI.escape(url_shared)}")
+		url = URI.parse("http://graph.facebook.com/?ids=#{URI.escape(url_shared)}")
 		#puts url
-		#data = Net::HTTP.get(url)
-		data = Net::HTTP.get(URI.parse("http://graph.facebook.com/?ids=#{URI.escape(url_shared)}"))
+		data = Net::HTTP.get(url)
+		#data = Net::HTTP.get(URI.parse("http://graph.facebook.com/?ids=#{URI.escape(url_shared)}"))
 	    #data = {'url_shared' => "asdf", 'shares' => 1}
-	    data = JSON.parse(data)	
+	    #data = JSON.parse(data)	
 	    #print("\n\n\n #{data} \n\n\n")
-    	if data[url_shared]['shares']
-    		return data[url_shared]['shares']
-    		return data['shares']
-    	else
+    	#if data[url_shared]['shares']
+    	#	return data[url_shared]['shares']
+    	# 	return data['shares']
+    	#else
     		return 0
-    	end
+    	#end
 	end
 end	
