46082c6a38f6577d505bafa79714c4dbdedc3e89
Eduardo Moreira <moreira.edu@hotmail.com>
2015-11-19 22:51:55 -0200

    Generate metrics of Sprint 4

--- tmp/metric_fu/output/flay.js
+++ tmp/metric_fu/output/flay.js
@@ -1,3 +1,3 @@
         var graph_title = 'Flay: duplication';
-        var graph_series = [{name: 'flay', data: [0,0,70,346,508,214]}];
-        var graph_labels = {"0":"9/22","1":"10/2","2":"10/22","3":"11/1","4":"11/3","5":"11/10"};
+        var graph_series = [{name: 'flay', data: [0,0,70,346,508,214,266]}];
+        var graph_labels = {"0":"9/22","1":"10/2","2":"10/22","3":"11/1","4":"11/3","5":"11/10","6":"11/17"};
