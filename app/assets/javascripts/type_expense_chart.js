fd62bc01b264667eeb8e5b1de84441b9392d7402
Marcelo Ferreira <marcelohpf@gmail.com>
2015-11-24 00:23:36 -0200

    Add filter to type expense
    Signed-off-by: matheus miranda <matheus-m-ml@hotmail.com>
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- app/assets/javascripts/type_expense_chart.js
+++ app/assets/javascripts/type_expense_chart.js
@@ -31,6 +31,36 @@ function drawTypeExpense(path,dataTypeExpenses){
     });
     showFilter(path,TYPE,updateType);
 }
+function updateColor(value,totalValue){
+    return {value: value, colorValue: value*100/totalValue};
+}
 function updateType(path,data){
     console.info("Update chart");
+    var chart = $('#'+TYPE+'.'+CHART).highcharts();
+    if (chart != undefined){
+        if (chart.series[0].data.length && data.length){
+
+            chart.series[0].data.forEach(function(element){
+                sizeData = data.length;
+                i =0;
+                var found = false;
+                do{ 
+                    if (data[i]['name'] == element.name ){
+                        found = true;
+                    }
+                }while(!found && ++i<sizeData);
+                if(found){
+                    element.update(updateColor(element.value+data[i]['value'],chart.series[0].tree.val));
+                    data.remove(i,1);
+                }                
+            });
+            data.forEach(function(element){
+                chart.series[0].addPoint(element);
+            });
+        }else{
+            data.forEach(function(element){
+                chart.series[0].addPoint(element);
+            });
+        }
+    }
 }
