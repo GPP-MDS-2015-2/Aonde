0ee4b6a9608fa4774388e7fd8658620d7e5f8090
Gabriel Silva <gabriel93.silva@gmail.com>
2015-11-24 22:37:28 -0200

    Adds env vars initialization

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Loads environment vars
+env_vars = File.join(Rails.root, 'config', 'env_vars.rb')
+load(env_vars) if File.exists?(env_vars)
+
 # Initialize the Rails application.
 Rails.application.initialize!
