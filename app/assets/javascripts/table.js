be555b9395e468fb752a1f52a17046fe2f8d1dbd
matheus miranda <matheus-m-ml@hotmail.com>
2015-11-30 17:16:11 -0200

    Fix problem with missing column
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>
    Signed-off-by: matheus miranda <matheus-m-ml@hotmail.com>

--- app/assets/javascripts/table.js
+++ app/assets/javascripts/table.js
@@ -4,10 +4,7 @@ $(document).ready( function(){
 		autoWidth: true,
 		pagingType: "full_numbers",
 		searching: true,
-		columnDefs:[{
-				visible: false,
-				targets:0
-				}],
+		columnDefs:[],
 		order: [1,'desc'],
 		//dom: "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
 		 dom: "<'top'>t<'bottom'pi>",
@@ -17,7 +14,30 @@ $(document).ready( function(){
 	});
 
 });
+
+//Define the options to dataTable and control that
+$(document).ready( function(){
+	$('#tableData1').dataTable({
+		autoWidth: true,
+		pagingType: "full_numbers",
+		searching: true,
+		columnDefs:[
+			{
+				visible: false,
+				targets:0
+			}],
+		order: [1,'desc'],
+		//dom: "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+		 dom: "<'top'>t<'bottom'pi>",
+		 oLanguage:{
+		 	sUrl: '//cdn.datatables.net/plug-ins/1.10.9/i18n/Portuguese-Brasil.json'
+		 }
+	});
+
+});
+
+
 function filterTable(name){
-	var table = $('#tableData').DataTable();
+	var table = $('#tableData1').DataTable();
 	table.column(0).search(name).draw();
 }
