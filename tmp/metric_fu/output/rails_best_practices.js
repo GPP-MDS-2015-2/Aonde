d650ddf9a3dd2243156910f20622976d7b2b86db
Eduardo Moreira <moreira.edu@hotmail.com>
2015-11-30 19:22:36 -0200

    Generate metrics of Sprint 5

--- tmp/metric_fu/output/rails_best_practices.js
+++ tmp/metric_fu/output/rails_best_practices.js
@@ -1,3 +1,3 @@
         var graph_title = 'Rails Best Practices: design problems';
-        var graph_series = [{name: 'rails_best_practices', data: [11,11,47,56,74,79,72]}];
-        var graph_labels = {"0":"9/22","1":"10/2","2":"10/22","3":"11/1","4":"11/3","5":"11/10","6":"11/17"};
+        var graph_series = [{name: 'rails_best_practices', data: [11,11,47,56,74,79,72,76]}];
+        var graph_labels = {"0":"9/22","1":"10/2","2":"10/22","3":"11/1","4":"11/3","5":"11/10","6":"11/17","7":"11/28"};
